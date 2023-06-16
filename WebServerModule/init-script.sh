d419f962be2dbe1e8c46d4d3cfefad731a379d4d
Ahmedben69 <123458760+Ahmedben69@users.noreply.github.com>
2023-06-16 15:43:11 +0200

    Update init-script.sh

--- WebServerModule/init-script.sh
+++ WebServerModule/init-script.sh
@@ -5,4 +5,4 @@ sudo apt install curl
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 #Creating container
-sudo docker run -d -p 80:1234 --name pytoon mowqa/pytoon
+sudo docker run -d -p 80:1234 --name pytoon bainos/brief14
