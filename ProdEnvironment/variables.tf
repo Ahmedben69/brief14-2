35ee6c85bffd3f9a7601a86d05d17a11b2291fb9
Ahmedben69 <123458760+Ahmedben69@users.noreply.github.com>
2023-06-16 15:45:58 +0200

    Create variables.tf

---
+++ ProdEnvironment/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+   description = "Azure subscription"
+   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+}
+
+variable "client_id" {
+   description = "Azure Client ID"
+   default = "34134380-c868-4fdc-8621-e5a6a3eeeddc"
+}
+
+variable "client_secret" {
+   description = "Azure Client Secret"
+   default = "k6t8Q~f4XwTSwecqc2CfSTmRapChksiEo8rbBbM7"
+}
+
+variable "tenant_id" {
+   description = "Azure Tenant ID"
+   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+}
